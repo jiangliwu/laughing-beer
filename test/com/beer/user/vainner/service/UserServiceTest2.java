6924ae8dde4477fcf8df980a004d2feaf84926f3
jiangliwu <jiangliwu.ipple@gmail.com>
2012-12-27 00:52:05 +0800

    添加了 spring3

---
+++ test/com/beer/user/vainner/service/UserServiceTest2.java
@@ -0,0 +1,30 @@
+package com.beer.user.vainner.service;
+
+
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.beer.user.vainner.model.User;
+
+
+
+public class UserServiceTest2 {
+
+	@Test
+	public void testAdd() {
+		ApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+		UserService userService = (UserService) factory.getBean("userService");
+		User user = new User();
+		user.setFirstName("1");
+		user.setLastName("2");
+		user.setGender("3");
+		user.setPassword("4");
+		user.setStudentId("5");
+		user.setUsername("6");
+		user.setPlayGameTimes(1);
+		userService.add(user);
+	}
+
+}
